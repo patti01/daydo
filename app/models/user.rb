b79ca117c6bf1ec591ea878ca5aef405384bc53b
Patricia Falandysz <pfalandysz@yahoo.com>
2019-07-11 10:28:32 +0200

    facebook setup finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,7 @@ class User < ApplicationRecord
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.new_with_session(params, session)
-    super.tap do
-      user
+    super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
       end
@@ -17,12 +16,11 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do
-      user
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.name = auth.info.name
+      user.image = auth.info.avatar
     end
   end
 end
